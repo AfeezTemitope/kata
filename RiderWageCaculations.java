0d179b727ed9991e81534c716ac8de66610fa82c
Tbelzbby <belloafeez28@gmail.com>
2024-04-13 08:12:00 +0100

    commit

--- RiderWageCaculations.java
+++ RiderWageCaculations.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class RiderWageCaculations{
     public static void main(String...args){
         Scanner scanner = new Scanner(System.in);    
-        List<Integer> rider = new ArrayList<>();
+        List<Object> result = new ArrayList<>();
         
         String continueInput = "Y";
 
@@ -12,17 +12,25 @@ public class RiderWageCaculations{
             System.out.println("Enter number of successful deliveries:");
             int successfulDeliveries = scanner.nextInt();
             scanner.nextLine();
-            
-            rider.add(successfulDeliveries);
 
             int ridersPayment = RidersWage.calculateRiderWage(successfulDeliveries);
+
+
             if (successfulDeliveries >= 100){
-                System.out.println("Test successful " + ridersPayment);
+                
+	    result.add("Delivery " + successfulDeliveries + " Test result (successful) : " + ridersPayment  );
+
             } else { 
-                System.out.println("Test failed " + ridersPayment);
+                
+	    result.add("Delivery " + successfulDeliveries + " Test result (failed) : " + ridersPayment  );
             }
             System.out.println("Do you want to enter more deliveries? (y/n)");
             continueInput = scanner.nextLine();
+	    }
+
+	    for (Object res : result) {
+            System.out.println(res);
+	   
         }
     }
 }
